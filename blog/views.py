5827e6f010e5bc03361fca23248f323148dcb4a5
[Anibal-programa] <[cursoprogramadorweb2022@gmail.com]>
2022-08-27 13:33:11 -0300

    1er subida de carpeta local a fork

--- blog/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from blog.models import Post
-
-# Create your views here.
-
-def blog(request):
-    
-    posts=Post.objects.all()
-    return render(request, "blog/blog.html", {"posts":posts})
